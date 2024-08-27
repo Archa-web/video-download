ec53012b17fda4d465126d0fd92620167f96a861
Archa-web <archams66@gmail.com>
2024-08-27 20:10:14 +0530

    new

--- dwnld/src/App.js
+++ dwnld/src/App.js
@@ -1,20 +1,25 @@
-import React from "react"
-import './App.css';
-import ReactDOM from 'react-dom';
-import VideoDownloader from './VideoDownloader';
-
-const App = () => {
-
-  return (
-    <div>
-      <h1>Video Download Example</h1>
-      <VideoDownloader />
-    </div>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from 'react'
+import "./App.css";
 
 
+const VIDEO = "http://ondemandwebcast.niyamasabha.org/videos/kla_11072024/110724-Qstn-1-1.mp4";
+
+funsction App() {
+    const downloadFile = {url} => {
+        const fileName = url.split("/"").pop();
+        const aTag = document.createElement('a');
+        aTag.href=url;
+        aTag.setAttribute('download',fileName);
+        document.body.appendChild(aTag);
+        aTag.click();
+        aTag.remove();
+    };
+    return (
+        <div className="App">
+            <button onClick={() => {downloadFile(VIDEO);
+}}>Download</button>
+        </div>
+    );
+}
 
 export default App;
