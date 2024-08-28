a05c0b212fbbbe53c5f8f4479f58564b8a3b86d1
Archa-web <archams66@gmail.com>
2024-08-28 17:20:08 +0530

    new

--- dwnld/src/Main.js
+++ dwnld/src/Main.js
@@ -6,17 +6,16 @@ const Main = () => {
       "http://ondemandwebcast.niyamasabha.org/videos/kla_11072024/110724-Qstn-1-1.mp4"; 
 
     try {
-      const response = await fetch(videoUrl);
+      const response = await fetch(videoUrl, {
+        mode: 'no-cors' // This will disable CORS restrictions, but limits access to the response
+      });
 
-      if (!response.ok) {
-        throw new Error(`Failed to download video: ${response.status} ${response.statusText}`);
-      }
       const blob = await response.blob();
       const url = window.URL.createObjectURL(blob);
 
       const link = document.createElement("a");
       link.href = url;
-      link.download = "your_video_name.mp4"; // Set the desired video file name
+      link.download = "webcasting_video.mp4"; // Set the desired video file name
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
