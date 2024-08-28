e9e3a19e215b2b95471ef4fd997d7959a10ca0ae
Archa-web <archams66@gmail.com>
2024-08-28 12:03:05 +0530

    new

--- dwnld/src/App.js
+++ dwnld/src/App.js
@@ -1,25 +1,18 @@
-import React from 'react'
-import "./App.css";
+import React from 'react';
+import Main from './Main';
 
 
-const VIDEO = "http://ondemandwebcast.niyamasabha.org/videos/kla_11072024/110724-Qstn-1-1.mp4";
-
-funsction App() {
-    const downloadFile = {url} => {
-        const fileName = url.split("/"").pop();
-        const aTag = document.createElement('a');
-        aTag.href=url;
-        aTag.setAttribute('download',fileName);
-        document.body.appendChild(aTag);
-        aTag.click();
-        aTag.remove();
-    };
-    return (
-        <div className="App">
-            <button onClick={() => {downloadFile(VIDEO);
-}}>Download</button>
-        </div>
-    );
+function App() {
+  return (
+    <div className="App">
+      <h1>Download MP4 Video</h1>
+      <Main />
+    </div>
+  );
 }
 
 export default App;
+
+
+
+//http://ondemandwebcast.niyamasabha.org/videos/kla_11072024/110724-Qstn-1-1.mp4
